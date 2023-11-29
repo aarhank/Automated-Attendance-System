1ddaa44f8c1e4a0100b64274cc5ed0ef7297cdc0
Aarhan <72205689+aarhank@users.noreply.github.com>
2023-11-29 12:14:29 +0530

    Update DashboardCards.js

--- src/components/dashboard/DashboardCards.js
+++ src/components/dashboard/DashboardCards.js
@@ -39,7 +39,7 @@ export default function DashboardCards({subject}) {
       body: payload,
     };
     
-    fetch("http://localhost:8086/lecture/create", requestOptions)
+    fetch("https://juit-attendance-acd05c65db38.herokuapp.com/lecture/create", requestOptions)
       .then(response => response.text())
       .then(async result => {
         
@@ -59,7 +59,7 @@ export default function DashboardCards({subject}) {
       method: 'GET',
       headers: myHeaders,
     };
-    fetch(`http://localhost:8086/lecture/subject/${data.id}`, requestOptions)
+    fetch(`https://juit-attendance-acd05c65db38.herokuapp.com/lecture/subject/${data.id}`, requestOptions)
       .then(response => response.text())
       .then(async result => {
         
